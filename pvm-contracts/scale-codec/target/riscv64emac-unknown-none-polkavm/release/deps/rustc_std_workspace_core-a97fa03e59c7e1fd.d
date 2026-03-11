aff42bd91634e35f38656c412d7961e1dc813e4f
Jerry Musaga <musagatechnology@gmail.com>
2026-03-11 14:39:23 +0100

    pvm integration

---
+++ pvm-contracts/scale-codec/target/riscv64emac-unknown-none-polkavm/release/deps/rustc_std_workspace_core-a97fa03e59c7e1fd.d
@@ -0,0 +1,7 @@
+/Users/musaga/GovMind/pvm-contracts/scale-codec/target/riscv64emac-unknown-none-polkavm/release/deps/librustc_std_workspace_core-a97fa03e59c7e1fd.rmeta: /Users/musaga/.rustup/toolchains/nightly-2024-11-19-x86_64-apple-darwin/lib/rustlib/src/rust/library/rustc-std-workspace-core/lib.rs
+
+/Users/musaga/GovMind/pvm-contracts/scale-codec/target/riscv64emac-unknown-none-polkavm/release/deps/librustc_std_workspace_core-a97fa03e59c7e1fd.rlib: /Users/musaga/.rustup/toolchains/nightly-2024-11-19-x86_64-apple-darwin/lib/rustlib/src/rust/library/rustc-std-workspace-core/lib.rs
+
+/Users/musaga/GovMind/pvm-contracts/scale-codec/target/riscv64emac-unknown-none-polkavm/release/deps/rustc_std_workspace_core-a97fa03e59c7e1fd.d: /Users/musaga/.rustup/toolchains/nightly-2024-11-19-x86_64-apple-darwin/lib/rustlib/src/rust/library/rustc-std-workspace-core/lib.rs
+
+/Users/musaga/.rustup/toolchains/nightly-2024-11-19-x86_64-apple-darwin/lib/rustlib/src/rust/library/rustc-std-workspace-core/lib.rs:
